380750fe162f80c6c1baa72841668a198d466d7c
cdziachan <christian.dziachan@gmail.com>
2021-12-26 14:17:45 -0800

    updated subnet_id for route table association

--- main.tf
+++ main.tf
@@ -126,9 +126,9 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-# Associate the public route table with the public subnets
+# Associate the public route table with the public subnet
 resource "aws_route_table_association" "public" {
-  subnet_id      = ["aws_subnet.Public_Subnet_az1.id", "aws_subnet.Public_Subnet_az2"]
+  subnet_id      = "${aws_subnet.Public_Subnet_az1.id}, ${aws_subnet.Public_Subnet_az2.id}"
   route_table_id = aws_route_table.public_rt.id
 }
 
